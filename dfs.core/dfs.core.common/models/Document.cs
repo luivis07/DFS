6f5637684a153f5f56cda14f6bfd391d7ddbc3a4
Luis Q <luivis07@gmail.com>
2023-04-04 21:11:18 -0400

    clean up

---
+++ dfs.core/dfs.core.common/models/Document.cs
@@ -0,0 +1,7 @@
+namespace dfs.core.common.models;
+public class Document
+{
+    public string Name { get; set; } = string.Empty;
+    public int Cost { get; set; }
+    public int QuantityAvailable { get; set; }
+}
